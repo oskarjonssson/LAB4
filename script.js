d70e65d27728d3c939a3c18ac9f5a96e562953ee
Dennis Björk <dennis.bjork87@gmail.com>
2017-11-28 13:08:55 +0100

    JS add key

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 let callback = function(event){
 
+  let apiBtn = document.getElementById('api-btn');
+  let apiOutput = document.getElementsByClassName('api-output')[0];
+  let req = new XMLHttpRequest();
 
-/* KOD HÄR */
+ // GET APU KEY JS KODE
+    function getApi(){
+      req.onreadystatechange = function(event) {
+        if (req.readyState == 4) {
+          let obj = JSON.parse(req.responseText);
+          apiOutput.innerHTML = obj.key;
+        }
+      };
+      req.open('GET', 'https://www.forverkliga.se/JavaScript/api/crud.php?requestKey');
+      req.send();
+    }
+
+    apiBtn.addEventListener('click', function() {
+      getApi()
+    });
+
 
 
 }
