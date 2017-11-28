081da7a7f65c82b35fd9ed1a2a36a7bc1ce70aab
Oskar Jonsson <oskarj97@hotmail.com>
2017-11-28 14:49:02 +0100

    Big changes click events

--- script.js
+++ script.js
@@ -19,10 +19,43 @@ apiBtn.addEventListener('click', function(event){
 
 // ADD A BOOK CODE
 
+let inputTitle = document.getElementById('input-title');
+let inputAuthor = document.getElementById('input-author');
+let addBtn = document.getElementById('add-btn');
+let listBooks = document.getElementById('list-books');
+
+
+// FUNCTION CLICK BUTTON CLICK EVENT
+function changeUrl(valueTitle, valueAuthor) {
+  let urlEdited = 'https://www.forverkliga.se/JavaScript/api/crud.php?op=insert&key=4tUkA' + '&title=' + valueTitle + '&author=' + valueAuthor;
+  console.log(urlEdited);
+     fetch(urlEdited)
+       .then(function(response) {
+         return response.json();
+       }).then(function(json) {
+         outputApi.innerHTML = "Status:"+json.status;
+         outputApi.innerHTML += " ID:"+ json.id;
+         console.log(json);
+       })
+};
+
+//SKAPAR LIST-ELEMENT OCH APPENDAR TILL listBooks
+function appendList(valueTitle, valueAuthor){
+  let liItem = document.createElement('li');
+  liItem.innerHTML = valueTitle + ', ' + valueAuthor;
+  listBooks.appendChild(liItem);
+};
 
 
 
 
+//BUTTON EVENT CLICK --
+addBtn.addEventListener('click', function(event){
+  let valueTitle = inputTitle.value;
+  let valueAuthor = inputAuthor.value;
+  changeUrl(valueTitle, valueAuthor);
+  appendList(valueTitle, valueAuthor);
+});
 
 
 
