6fa71cf3ebdbc671b927cf777b27518d7cb47036
Oskar Jonsson <oskarj97@hotmail.com>
2017-12-01 12:07:53 +0100

    Big changes add, show and delete book

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let callback = function(event){
 let url = 'https://www.forverkliga.se/JavaScript/api/crud.php?requestKey';
 let outputApi = document.getElementsByClassName('api-output')[0];
 let apiBtn = document.getElementById('api-btn');
-function getApi(){
+let getApi = function(){
   fetch(url)
   .then(function(response) {
     return response.json();
@@ -13,77 +13,71 @@ function getApi(){
   })
 };
 
-apiBtn.addEventListener('click', function(event){
-  getApi();
-});
-
-
-
 // ADD A BOOK CODE
 let inputTitle = document.getElementById('input-title');//INPUT TITLE
 let inputAuthor = document.getElementById('input-author');//INPUT AUTHOR
 let addBtn = document.getElementById('add-btn'); //ADD BOOK BUTTON
-let listBooks = document.getElementById('list-books'); // LISTA/UL FOR BOOKS
-let showBtn = document.getElementById('show-btn');//SHOW BOOKS BUTTON
+let bookOutput = document.getElementsByClassName('bookOutput')[0]; // DIV FOR BOOKS
+let inputDelete = document.getElementById('input-delete');//INPUT DELETE BOOK
+let deleteBtn = document.getElementById('btn-delete');//BUTTON DELETE BOOK
 
-
-// FUNCTION CLICK BUTTON CLICK EVENT
-//INPUT FOR QUERYSTRINGS
-function changeUrl(valueTitle, valueAuthor) {
-  let urlEdited = 'https://www.forverkliga.se/JavaScript/api/crud.php?op=insert&key=4tUkA' + '&title=' + valueTitle + '&author=' + valueAuthor;
-  console.log(urlEdited);
-     fetch(urlEdited)
-       .then(function(response) {
-         return response.json();
-       }).then(function(json) {
-         outputApi.innerHTML = "Status:"+json.status;
-         outputApi.innerHTML += " ID:"+ json.id;
-         console.log(json);
-       })
-};
-
-let check = function() {
-  let valueTitle = inputTitle.value;//INPUT VALUE TITLE
-  let valueAuthor = inputAuthor.value;//INPUT VALUE AUTHOR
-  let urlEdited = 'https://www.forverkliga.se/JavaScript/api/crud.php?op=insert&key=4tUkA' + '&title=' + valueTitle + '&author=' + valueAuthor;
-  console.log(urlEdited);
-     fetch(urlEdited)
-       .then(function(response) {
-         return response.json();
-       }).then(function(json) {
-         console.log(json);
-         if(json.status === 'success'){//IF API RETURNS STATUS SUCCESS - CREATES NEW BOOK FROM INPUT
-           let liItem = document.createElement('li');
-           liItem.innerHTML = valueTitle + ', ' + valueAuthor;
-           listBooks.appendChild(liItem);
-           listBooks.style.display = 'none';
-         }else {
-           console.log('ERROR');//ERROR HANDLING - DISPLAYS IF ERROR FROM API
-         }
-       })
-};
+//VIEW ALL BOOKS
+let viewDataFunction = function() {
+  let infoDiv = document.getElementById('infoDiv');
+  let url = 'https://www.forverkliga.se/JavaScript/api/crud.php?op=select&key=GxtKv';
+  fetch(url)
+    .then(function(response) {
+      return response.json();
+    }).then(function(json) {
+      console.log(json);
+        if(json.status === 'success'){
+        document.getElementById('div-text').innerHTML = "";
+        for(i = 0; i < json.data.length; i++) {
+          document.getElementById('div-text').innerHTML +=
+          "Author: " + json.data[i].author +
+          " Title: " + json.data[i].title +
+          " ID: " + json.data[i].id +
+          " Updated: " + json.data[i].updated + "<br>";
+        }
+      }else{
+        document.getElementById('div-text').innerHTML += 'Error loading books - Please refresh the page';
+      }
+      console.log(json);
+    })
+}
+viewDataFunction();
 
 
 //SKAPAR LIST-ELEMENT OCH APPENDAR TILL listBooks
-function appendList(valueTitle, valueAuthor){
-
-  let liItem = document.createElement('li');
-  liItem.innerHTML = valueTitle + ', ' + valueAuthor;
-  listBooks.appendChild(liItem);
+let createBook = function() {
+  let valueTitle = inputTitle.value;//INPUT VALUE TITLE
+  let valueAuthor = inputAuthor.value;//INPUT VALUE AUTHOR
+  let urlEdited = 'https://www.forverkliga.se/JavaScript/api/crud.php?op=insert&key=GxtKv' + '&title=' + valueTitle + '&author=' + valueAuthor;
+  console.log(urlEdited);
+     fetch(urlEdited)
+       .then(function(response) {
+         return response.json();
+       }).then(function(json) {
+         console.log(json);
+       })
 };
+//DELETE BOOK BY ID
+let deleteBook = function() {
+let valueDelete = inputDelete.value; //INPUT VALUE ID
+let url = 'https://www.forverkliga.se/JavaScript/api/crud.php?op=delete&key=GxtKv' + '&id=' + valueDelete;
+fetch(url)
+  .then(function(response) {
+    return response.json();
+  }).then(function(json) {
+    viewDataFunction();
+  });
+}
 
-//BUTTON EVENT CLICK -- appendList & changeUrl
-addBtn.addEventListener('click', check);
-//SHOW LIST / HIDE LIST
-showBtn.addEventListener('click', function(event){
-  if(listBooks.style.display === 'none'){
-    listBooks.style.display = 'block';
-    showBtn.innerHTML = 'Hide books'
-  }else{
-    listBooks.style.display = 'none';
-    showBtn.innerHTML = 'Show books'
-  }
-});
+// EVENTS
+apiBtn.addEventListener('click', getApi);//GET API - CLICK EVENT
+addBtn.addEventListener('click', createBook);//ADD BOOK - CLICK EVENT
+deleteBtn.addEventListener('click', deleteBook);//DELETE BOOK BY ID
+
 
 }
 window.addEventListener('load', callback);
