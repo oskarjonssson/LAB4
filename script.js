8cf876f09a672055e099efb094b82575ba1dba1d
Dennis Björk <dennis.bjork87@gmail.com>
2017-12-13 23:42:36 +0100

    update on viewlist

--- script.js
+++ script.js
@@ -39,6 +39,7 @@ let viewDataFunction = function() {
           //" Updated: " + json.data[i].updated + "<br>";
         }
       }else{
+        viewDataFunction()
         bookTextList.innerHTML += json.message + " " + "- Please refresh the page";
         counter += 1;
         counterOutput.innerHTML ='ERRORS: ' + counter;
@@ -70,11 +71,6 @@ let createBook = function(googleTitle, googleAuthor) {
          }
        })
 };
-
-
-
-
-
 
 //DELETE BOOKS
 let inputDelete = document.getElementById('input-delete');//INPUT DELETE BOOK
